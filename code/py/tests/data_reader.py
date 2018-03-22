e02395b61b6303edc2f6e7c24050d6edb791ac78
poore <py100@live.cn>
2018-03-22 12:33:05 +0800

    init without all sensitive files

---
+++ code/py/tests/data_reader.py
@@ -0,0 +1,10 @@
+# -*- coding:utf-8 -*-
+file = open('../data/2016-11-01/85001/wda_mooc/wda_mooc_85001.txt')
+for line in file.readlines():
+    print '------------------'
+    # print len(line.decode('utf-8').split('\t'))
+    # print line.decode('utf-8').split('\t')[16]
+    # for word in line.decode('utf-8').split(chr(1)):
+    #    print word,
+    print
+
